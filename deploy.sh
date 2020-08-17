5abb1b57877507d345dc1645b16866c1a505ae24
Med <mohamedcra@yahoo.fr>
2020-08-17 06:28:21 +0200

    unwanted files

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rsync -av ./ nb8tr_plasticrcube@plasticrcube.fr:~/plasticrcube --include=public/bundles --include=public/.htaccess --include=vendor --exclude-from=.gitignore --exclude=".*"
